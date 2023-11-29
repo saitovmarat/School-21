abe4a9bbb49430fd7028b356433b9b224ce8e507
Saitov Marat <112775080+saitovmarat@users.noreply.github.com>
2023-11-29 23:06:42 +0300

    Delete C4_s21_math directory

--- C4_s21_math/src/Functions/s21_abs.c
+++
@@ -1,9 +0,0 @@
-#include "../s21_math.h"
-/**
- * @brief Get absolute value of x.
- *
- * @param x Input value.
- * @return Absolute value of x.
- * @retval int
- */
-int s21_abs(int x) { return (x < 0) ? -x : x; }
